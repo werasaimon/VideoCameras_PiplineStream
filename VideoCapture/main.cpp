876fb7daebd4ae26370f71295903eb46b039a2a0
werasaimon <nicemankit@gmail.com>
2021-09-10 02:52:17 +0200

    H.264 | JPEG

--- VideoCapture/main.cpp
+++ VideoCapture/main.cpp
@@ -12,21 +12,22 @@ const int inpHeight = 480;
 
 
 
-//    std::string gst_pipeline_cams[]
-//    {
-//        "nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480,format=(string)NV12, framerate=(fraction)30/1 !"
-//                                           " nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink",
+std::string gst_pipeline_cams[]
+{
+    "nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480,format=(string)NV12, framerate=(fraction)30/1 !"
+    " nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink",
 
-//        "nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480,format=(string)NV12, framerate=(fraction)30/1 !"
-//                                           " nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink",
+    "nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=(int)640, height=(int)480,format=(string)NV12, framerate=(fraction)30/1 !"
+    " nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink",
 
-//        "v4l2src device=/dev/video2 ! video/x-raw, width=(int)640, height=(int)480, format=(string)YUY2, framerate=(fraction)30/1 !"
-//        " videoconvert ! video/x-raw, width=(int)640, height=(int)480, format=(string)RGB ! videoconvert ! appsink",
+    "v4l2src device=/dev/video2 ! video/x-raw, width=(int)640, height=(int)480, format=(string)YUY2, framerate=(fraction)30/1 !"
+    " videoconvert ! video/x-raw, width=(int)640, height=(int)480, format=(string)RGB ! videoconvert ! appsink",
 
-//        "v4l2src device=/dev/video3 ! video/x-raw, width=(int)640, height=(int)480, format=(string)YUY2, framerate=(fraction)30/1 !"
-//                                         " videoconvert ! video/x-raw, width=(int)640, height=(int)480, format=(string)RGB ! videoconvert ! appsink"
-//    };
+    "v4l2src device=/dev/video3 ! video/x-raw, width=(int)640, height=(int)480, format=(string)YUY2, framerate=(fraction)30/1 !"
+    " videoconvert ! video/x-raw, width=(int)640, height=(int)480, format=(string)RGB ! videoconvert ! appsink"
+};
 
+/**
 
 std::string gst_pipeline_cams[]
 {
@@ -42,23 +43,26 @@ std::string gst_pipeline_cams[]
 
 };
 
-
-//std::string lisen_src_gst_send_video( std::string ip , int port , int fps=30)
-//{
-//    std::string gst_video_send = "appsrc ! videoconvert ! video/x-raw,format=YUY2,"
-//                                 "width=(int)640"
-//                                 ",height=(int)480"
-//                                 ",framerate=" + std::to_string(fps) + "/1 ! "
-//                                 "jpegenc ! rtpjpegpay ! "
-//                                 "udpsink host=" + ip +
-//                                 " port=" + std::to_string(port) +
-//                                 " sync=false async=false";
-//    return gst_video_send;
-//}
+/**/
 
 
+std::string lisen_src_gst_JPEG_send_video( std::string ip , int port , int fps=30)
+{
+    std::string gst_video_send = "appsrc ! videoconvert ! video/x-raw,format=YUY2,"
+                                 "width=(int)640"
+                                 ",height=(int)480"
+                                 ",framerate=" + std::to_string(fps) + "/1 ! "
+                                 "jpegenc ! rtpjpegpay ! "
+                                 "udpsink host=" + ip +
+                                 " port=" + std::to_string(port) +
+                                 " sync=false async=false";
+    return gst_video_send;
+}
 
-std::string lisen_src_gst_send_video( std::string ip , int port , int fps=30)
+
+
+
+std::string lisen_src_gst_H264_send_video( std::string ip , int port , int fps=30)
 {
     std::string gst_video_send = "appsrc ! videoconvert ! "
                                  "x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! queue ! rtph264pay ! "
@@ -67,6 +71,18 @@ std::string lisen_src_gst_send_video( std::string ip , int port , int fps=30)
                                  " sync=false async=false";
     return gst_video_send;
 }
+
+    /**
+    std::string lisen_src_gst_H264_send_video( std::string ip , int port , int fps=30)
+    {
+        std::string gst_video_send = "appsrc ! videoconvert ! "
+                                     "x264enc tune=zerolatency bitrate=500 speed-preset=superfast ! queue ! rtph264pay ! "
+                                     "udpsink host=" + ip +
+                                     " port=" + std::to_string(port) +
+                                     " sync=false async=false";
+        return gst_video_send;
+    }
+    /**/
 
 
 //std::string lisen_src_gst_send_video( std::string ip , int port , int fps=30)
@@ -90,14 +106,32 @@ int main()
     std::string ip;
     std::cout << "ip addres : "; std::cin >> ip;
 
-    int n = 3;
+    int n_cameras;
+    std::cout << "counts of cameras: "; std::cin >> n_cameras;
+
+    int codec;
+    std::cout << "codec H264=0 JPEG=1: "; std::cin >> codec;
+
+    const int n = n_cameras;
     std::vector<std::thread> sthreads;
     cv::VideoCapture videoCapture[n];
     cv::VideoWriter  videoWrite[n];
     for (int i = 0; i < n; ++i)
     {
         videoCapture[i].open(gst_pipeline_cams[i]);
-        videoWrite[i].open(lisen_src_gst_send_video(ip,5000+i),0, (double)30, cv::Size(640, 480), true);
+
+        if(codec == 0)
+        {
+            int fourcc = cv::VideoWriter::fourcc('H','2','6','4');
+            videoWrite[i].open(lisen_src_gst_H264_send_video(ip,5000+i),fourcc, (double)30, cv::Size(640, 480), true);
+        }
+        else if(codec == 1)
+        {
+
+            int fourcc = cv::VideoWriter::fourcc('J','P','E','G');
+            videoWrite[i].open(lisen_src_gst_JPEG_send_video(ip,5000+i),fourcc, (double)30, cv::Size(640, 480), true);
+        }
+
 
         if (!videoCapture[i].isOpened() || !videoWrite[i].isOpened())
         {
@@ -120,7 +154,7 @@ int main()
 
 void stream_write(cv::VideoCapture* videoCap, cv::VideoWriter* videoWrite)
 {
-    cv::Mat frame;
+    cv::Mat frame; int n_frame=0;
     while(videoCap->isOpened())
     {
         // wait for a new frame from camera and store it into 'frame'
@@ -135,13 +169,13 @@ void stream_write(cv::VideoCapture* videoCap, cv::VideoWriter* videoWrite)
         if(videoWrite->isOpened())
         {
             videoWrite->write(frame);
-           // std::cout << "write\n";
+            std::cout << "write frame " << n_frame++ << "\r";
         }
 
         //show live and wait for a key with timeout long enough to show images
         //        imshow("Live", frame);
-        //        if (cv::waitKey(5) >= 0)
-        //            break;
+        if (cv::waitKey(5) >= 0)
+            break;
     }
 }
 
